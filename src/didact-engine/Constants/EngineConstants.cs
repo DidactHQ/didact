b5ac728be1d396500dd79a0f4df61a5ac26d6e62
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-08-08 17:22:12 -0500

    Created EngineConstants.cs in didact-engine

---
+++ src/didact-engine/Constants/EngineConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace DidactEngine.Constants
+{
+    public static class EngineConstants
+    {
+        public static class CorsPolicyNames
+        {
+            public const string Development = "DevelopmentCors";
+            public const string Staging = "StagingCors";
+            public const string Production = "ProductionCors";
+        }
+    }
+}
