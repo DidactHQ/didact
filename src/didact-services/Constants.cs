f4527c1cf316d47d5e8e8062bc4b178283e529fa
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-08-08 00:58:29 -0500

    Created Constants class in didact-services

---
+++ src/didact-services/Constants.cs
@@ -0,0 +1,35 @@
+﻿namespace DidactServices
+{
+    public static class Constants
+    {
+        public static class ApplicationNames
+        {
+            public const string DidactUi = "Didact UI";
+            public const string DidactEngine = "Didact Engine";
+            public const string DidactCli = "Didact CLI";
+        }
+
+        public static class ApplicationConfigurationFileNames
+        {
+            public const string DidactUiSettings = "uisettings.json";
+            public const string DidactEngineSettings = "enginesettings.json";
+        }
+
+        public static class Defaults
+        {
+            public const string DefaultBuildEnvironment = BuildEnvironments.Production;
+        }
+
+        public static class Keys
+        {
+            public const string BuildEnvironment = "DIDACT_BUILD_ENVIRONMENT";
+        }
+
+        public static class BuildEnvironments
+        {
+            public const string Development = "Development";
+            public const string Staging = "Staging";
+            public const string Production = "Production";
+        }
+    }
+}
