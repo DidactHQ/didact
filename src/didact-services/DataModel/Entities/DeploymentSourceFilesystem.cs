e770dfe2db8b62186fc5f9bfb1dfd9db88eefab1
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-11-12 02:32:05 -0600

    Created new deployment entities

---
+++ src/didact-services/DataModel/Entities/DeploymentSourceFilesystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DidactServices.DataModel.Entities
+{
+    public class DeploymentSourceFilesystem
+    {
+        public long DeploymentSourceFilesystemId { get; set; }
+
+        public long DeploymentId { get; set; }
+
+        public long EnvironmentId { get; set; }
+
+        public string FolderPath { get; set; } = null!;
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime Updated { get; set; }
+
+        public string UpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public byte[] RowVersion { get; set; } = null!;
+    }
+}
