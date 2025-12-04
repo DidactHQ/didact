80883d7cfb80c4254731e4013848068a350bdea3
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-04 17:36:54 -0600

    Workers wip in didact-engine

---
+++ src/didact-engine/Workers/WorkerContext.cs
@@ -0,0 +1,17 @@
+﻿using DidactCore.Flows;
+
+namespace DidactEngine.Workers
+{
+    public class WorkerContext
+    {
+        public FlowContext? FlowContext { get; set; }
+
+        public FlowRunContext? FlowRunContext { get; set; }
+
+        public EnvironmentContext? EnvironmentContext { get; set; }
+
+        public DeploymentContext? DeploymentContext { get; set; }
+
+        public IFlow? FlowInstance { get; set; }
+    }
+}
--- src/didact-engine/Workers/WorkersService.cs
+++ src/didact-engine/Workers/WorkersService.cs
@@ -6,12 +6,32 @@
         {
         }
 
-        public async Task PollDatabaseForCompatibleFlowRunAsync()
+        public async Task PollDatabaseForCompatibleFlowRunAsync(CancellationToken cancellationToken)
         {
             await Task.CompletedTask;
         }
 
-        public async Task ExecuteFlowRunAsync()
+        public async Task InstantiateFlowAsync(WorkerContext workerContext, CancellationToken cancellationToken)
+        {
+
+        }
+
+        public async Task PollFlowRunCancellationAsync(WorkerContext workerContext, CancellationToken compositeCancellationToken)
+        {
+
+        }
+
+        public async Task ExecuteFlowRunTimeoutCountdownAsync(WorkerContext workerContext, CancellationToken compositeCancellationToken)
+        {
+
+        }
+
+        public async Task ExecuteFlowRunAsync(WorkerContext workerContext, CancellationToken compositeCancellationToken)
+        {
+
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             await Task.CompletedTask;
         }
