04b164eee81de1a6f68ddb951338d6fef5442971
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-04 17:33:33 -0600

    Created engine defaults constants class in didact-engine

---
+++ src/didact-engine/Constants/Defaults.cs
@@ -0,0 +1,15 @@
+﻿namespace DidactEngine.Constants
+{
+    public static class Defaults
+    {
+        public static class DefaultModuleIntervalDelays
+        {
+            public const int Plugins = 120000;
+            public const int Scheduler = 5000;
+            public const int Workers = 0;
+            public const int Licensing = 1800000;
+            public const int EngineLogger = 0;
+            public const int FlowRunLogger = 0;
+        }
+    }
+}
