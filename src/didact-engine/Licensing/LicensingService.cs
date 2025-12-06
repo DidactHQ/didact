87d6b90646f0b04bc64d549668ea9772061350ac
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-06 17:01:40 -0600

    Created LicensingService in didact-engine

---
+++ src/didact-engine/Licensing/LicensingService.cs
@@ -0,0 +1,12 @@
+﻿namespace DidactEngine.Licensing
+{
+    public class LicensingService
+    {
+        private readonly ILogger<LicensingService> _logger;
+
+        public LicensingService(ILogger<LicensingService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
