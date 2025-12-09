94e4e1191380e42855bdc3581508a2888bfe5bd1
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-08 23:01:44 -0600

    Scheduler wip in didact-engine

---
+++ src/didact-engine/Scheduler/SchedulerModule.cs
@@ -0,0 +1,28 @@
+﻿using DidactEngine.Constants;
+using DidactEngine.Modules;
+
+namespace DidactEngine.Scheduler
+{
+    public class SchedulerModule : IModule
+    {
+        private readonly SchedulerService _schedulerService;
+
+        public string Name => EngineConstants.ModuleNames.Scheduler;
+
+        public bool Enabled { get; set; } = true;
+
+        public int Concurrency { get; set; } = 1;
+
+        public int IntervalDelay { get; set; } = Defaults.DefaultModuleIntervalDelays.Scheduler;
+
+        public SchedulerModule(SchedulerService schedulerService)
+        {
+            _schedulerService = schedulerService;
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
---
+++ src/didact-engine/Scheduler/SchedulerService.cs
@@ -0,0 +1,12 @@
+﻿namespace DidactEngine.Scheduler
+{
+    public class SchedulerService
+    {
+        private readonly ILogger<SchedulerService> _logger;
+
+        public SchedulerService(ILogger<SchedulerService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
