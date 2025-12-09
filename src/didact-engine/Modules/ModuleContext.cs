4c698f221a25b8ce7a344d1eadcf3fe069c3f33f
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-08 23:34:57 -0600

    Modules wip in didact-engine

---
+++ src/didact-engine/Modules/ModuleContext.cs
@@ -0,0 +1,22 @@
+﻿namespace DidactEngine.Modules
+{
+    public sealed class ModuleContext
+    {
+        public IModule Module { get; }
+
+        public CancellationToken CancellationToken { get; }
+
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public int WorkerIndex { get; }
+
+        public string Name => $"{Module.Name} module (index: {WorkerIndex} | id: {Id})";
+
+        public ModuleContext(IModule module, int workerIndex, CancellationToken cancellationToken)
+        {
+            Module = module;
+            WorkerIndex = workerIndex;
+            CancellationToken = cancellationToken;
+        }
+    }
+}
--- src/didact-engine/Modules/ModuleSupervisor.cs
+++ src/didact-engine/Modules/ModuleSupervisor.cs
@@ -17,7 +17,9 @@ namespace DidactEngine.Modules
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // Create a composite cancellation token that covers an engine shutdown event from the runtime as well as from manual engine polling.
+            /* Create a composite cancellation token that covers an engine shutdown event from the runtime
+             * as well as from manual engine polling.
+             */
             using var compositeCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken, _engineService.CancellationToken);
             var compositeCancellationToken = compositeCancellationTokenSource.Token;
 
@@ -25,29 +27,33 @@ namespace DidactEngine.Modules
                 .Where(m => m.Enabled)
                 .SelectMany(m =>
                     Enumerable.Range(0, m.Concurrency)
-                                .Select(_ => RunModuleLoopAsync(m, compositeCancellationToken)))
+                                .Select(i => RunModuleLoopAsync(new ModuleContext(m, i + 1, compositeCancellationToken))))
                 .ToList();
 
             await Task.WhenAll(moduleTasks);
         }
 
-        private async Task RunModuleLoopAsync(IModule module, CancellationToken ct)
+        private async Task RunModuleLoopAsync(ModuleContext moduleContext)
         {
-            while (!ct.IsCancellationRequested && module.Enabled)
+            _logger.LogInformation("{name} starting {supervisorName} execution loop...", moduleContext.Name, nameof(ModuleSupervisor));
+
+            while (!moduleContext.CancellationToken.IsCancellationRequested && moduleContext.Module.Enabled)
             {
                 try
                 {
-                    await module.ExecuteAsync(ct);
+                    await moduleContext.Module.ExecuteAsync(moduleContext.CancellationToken);
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "{Name} module crashed. Restarting...", module.Name);
-                    await Task.Delay(module.IntervalDelay, ct);
+                    _logger.LogError(ex, "{Name} crashed. Restarting...", moduleContext.Name);
+                    await Task.Delay(moduleContext.Module.IntervalDelay, moduleContext.CancellationToken);
                     continue; // restart the loop (revives the module)
                 }
 
-                await Task.Delay(module.IntervalDelay, ct);
+                await Task.Delay(moduleContext.Module.IntervalDelay, moduleContext.CancellationToken);
             }
+
+            _logger.LogWarning("{name} {supervisorName} execution loop stopped.", moduleContext.Name, nameof(ModuleSupervisor));
         }
     }
 }
