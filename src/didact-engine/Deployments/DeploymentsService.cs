3d92cb05c4203cf5c5b845822dde5f5abd71ceac
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-12-12 13:51:16 -0600

    DeploymentsService wip

---
+++ src/didact-engine/Deployments/DeploymentsService.cs
@@ -0,0 +1,24 @@
+﻿using DidactCore.Deployments;
+
+namespace DidactEngine.Deployments
+{
+    public class DeploymentsService
+    {
+        private readonly ILogger<DeploymentsService> _logger;
+
+        public DeploymentsService(ILogger<DeploymentsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task FetchMissingDeploymentsAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task FetchDeploymentFromSourceAsync(DeploymentContext deploymentContext, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
