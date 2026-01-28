282b426abfea50cc4251e21a4a9fdcc4e3833e55
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-27 18:49:06 -0600

    Removed old didact-services proj and associated files

--- src/didact-services/Deployments/DeploymentContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DidactCore.Deployments
-{
-    public class DeploymentContext
-    {
-        public long DeploymentId { get; set; }
-
-        public string? Name { get; set; }
-    }
-}
