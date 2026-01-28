d68af58036eb9c469ee6c93e05cedd3e3aff324a
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-27 18:36:11 -0600

    mapper wip in didact-services

---
+++ src/didact-services/Mapping/Mapper.cs
@@ -0,0 +1,13 @@
+﻿using DidactServices.FlowRuns;
+using DidactServices.Workers;
+
+namespace DidactServices.Mapping
+{
+    public static class Mapper
+    {
+        public static WorkerContext MapToWorkerContext(this FlowRunWorkerContextDto flowRunWorkerContextDto)
+        {
+            return new WorkerContext();
+        }
+    }
+}
