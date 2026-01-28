d46d4763cb2976ff4b8ed413e2277ac9b654fc26
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-27 18:05:47 -0600

    Created SystemContext class in didact-engine

---
+++ src/didact-engine/System/SystemContext.cs
@@ -0,0 +1,9 @@
+﻿namespace DidactEngine.System
+{
+    public class SystemContext
+    {
+        public string MachineName { get; set; } = Environment.MachineName;
+
+        public int ProcessId { get; set; } = Environment.ProcessId;
+    }
+}
