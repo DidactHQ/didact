a44448552f9d12119013f4082f12c1ee67d91d76
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-01-27 18:35:55 -0600

    flowrun classes wip in didact-services

---
+++ src/didact-services/FlowRuns/FlowRunWorkerContextDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DidactServices.FlowRuns
+{
+    public class FlowRunWorkerContextDto
+    {
+        public long FlowRunId { get; set; }
+
+        public long FlowVersionId { get; set; }
+
+        public string FlowVersion { get; set; } = null!;
+
+        public string? JsonPayload { get; set; }
+
+        public int TimeoutSeconds { get; set; }
+    }
+}
