1684b6a3a09fa462c5deea742947dbc482ec411b
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-05-23 13:11:48 -0500

    Created SystemTriggers constants in didact-primitives

---
+++ src/didact-primitives/Constants/SystemTriggers.cs
@@ -0,0 +1,9 @@
+﻿namespace DidactPrimitives.Constants
+{
+    public static class SystemTriggers
+    {
+        public const string Api = "API";
+        public const string Dashboard = "Dashboard";
+        public const string Cli = "CLI";
+    }
+}
