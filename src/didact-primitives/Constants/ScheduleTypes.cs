f4d81eb16d68433fb49fa6f02729040bb0045e5f
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-05-23 13:13:26 -0500

    Created ScheduleTypes in didact-primitives

---
+++ src/didact-primitives/Constants/ScheduleTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace DidactPrimitives.Constants
+{
+    public static class ScheduleTypes
+    {
+        public const string Cron = "Cron";
+        public const string Interval = "Interval";
+        public const string OneTime = "OneTime";
+        public const string ICal = "ICal";
+    }
+}
