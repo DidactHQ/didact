5a500ef451fe4c9cafe8d965bb623d7c24afe3b1
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-05-28 03:27:44 -0500

    Created wip IDirectory in didact-core

---
+++ src/didact-core/Director/IDirector.cs
@@ -0,0 +1,24 @@
+﻿using DidactCore.Steps;
+using System;
+using System.Threading.Tasks;
+
+namespace DidactCore.Director
+{
+    /// <summary>
+    /// Utility service for dispatching actions against the Didact platform from within the context of a FlowRun.
+    /// </summary>
+    public interface IDirector
+    {
+        /// <summary>
+        /// Creates a new FlowRun for the specified Flow to be linked as a child FlowRun.
+        /// </summary>
+        /// <param name="flowName">The name of the flow to create a subflow run for.</param>
+        /// <param name="jsonPayload">The JSON payload to pass to the subflow run.</param>
+        /// <param name="executeAt">The optional execution time for the subflow run.</param>
+        Task CreateSubflowRunAsync(string flowName, string? jsonPayload, DateTime? executeAt);
+        
+        Task ExecuteStepAsync(string name, Func<IStepExecutionContext, Task> function);
+
+        Task<TOutput> ExecuteStepAsync<TOutput>(string name, Func<IStepExecutionContext, Task<TOutput>> function);
+    }
+}
