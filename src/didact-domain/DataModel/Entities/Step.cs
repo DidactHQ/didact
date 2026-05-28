66fa6fc762a0f080716f6817aa3b791e81086dfe
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-05-28 03:26:45 -0500

    Created step entity in didact-domain

---
+++ src/didact-domain/DataModel/Entities/Step.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DidactDomain.DataModel.Entities
+{
+    public class Step
+    {
+        public long StepId { get; set; }
+
+        public long FlowRunId { get; set; }
+
+        public long EnvironmentId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateTime? ExecutionStartedAt { get; set; }
+
+        public DateTime? ExecutionEndedAt { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public DateTime Updated { get; set; }
+
+        public string UpdatedBy { get; set; } = null!;
+
+        public bool Active { get; set; }
+    }
+}
