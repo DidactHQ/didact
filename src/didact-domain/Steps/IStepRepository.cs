649340b7708e8210dfc882f02f8827358b5df2af
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-05-28 03:27:08 -0500

    Created IStepRepository in didact-domain

---
+++ src/didact-domain/Steps/IStepRepository.cs
@@ -0,0 +1,12 @@
+﻿using DidactCore.Steps;
+using System.Threading.Tasks;
+
+namespace DidactDomain.Steps
+{
+    public interface IStepRepository
+    {
+        Task<IStepContext> GetStepAsync(long stepId);
+
+        Task<IStepContext> GetOrCreateStepAsync(long flowRunId, string stepName);
+    }
+}
