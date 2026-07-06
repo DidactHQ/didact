8840e2ea87cc9c8156c0f7e1de2a29673a9d0f0e
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-06 01:09:06 -0500

    Created IFlowRepository in didact-domain

---
+++ src/didact-domain/Flows/IFlowRepository.cs
@@ -0,0 +1,14 @@
+﻿using DidactCore.Flows;
+using System.Threading.Tasks;
+
+namespace DidactDomain.Flows
+{
+    public interface IFlowRepository
+    {
+        Task<IFlowContext> GetFlowAsync(long flowId);
+
+        Task<IFlowContext> GetFlowAsync(string flowName, string environmentName);
+
+        Task DeactivateFlowAsync(long flowId);
+    }
+}
