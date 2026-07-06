73b60bc6a982f35628ccf6d20d351a401cffe6a2
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-06 01:11:41 -0500

    SomeFlow major wip

--- src/didact-core/SomeFlow.cs
+++ src/didact-core/SomeFlow.cs
@@ -1,20 +1,37 @@
 ﻿using DidactCore.Flows;
-using DidactCore.Triggers;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DidactCore
 {
     public class SomeFlow : IFlow
     {
-        public Task<IFlowConfigurationContext> ConfigureAsync(IFlowConfigurationContext context)
+        public Task ConfigureAsync(IFlowConfigurationContext context)
         {
             context.Configurator
-                .WithName("An example flow")
+                .WithName("scraper")
                 .WithDescription("A sample flow")
                 .AsVersion("1.0.0")
-                .WithCronScheduleTrigger(new CronScheduleTrigger("test"));
-            
-            return Task.FromResult(context);
+                .DeferExecutionBy(TimeSpan.FromMinutes(5))
+                .WithRetryPolicy(retry => retry
+                    .UseFixedInterval(TimeSpan.FromSeconds(30))
+                    .WithMaxAttempts(3))
+
+                // Simple schedule methods
+                .UseCronSchedule(name: "recurring-schedule", cronExpression: "0 0 * * *")
+                .UseOneTimeSchedule(name: "special-schedule", executeAt: DateTime.UtcNow.AddDays(15))
+
+                // Advanced schedule builders
+                .UseCronSchedule(cron => cron
+                    .WithName("recurring-schedule")
+                    .WithCronExpression("0 0 * * *"))
+                .UseOneTimeSchedule(oneTime => oneTime
+                    .WithName("special-schedule")
+                    .ScheduleExecutionAt(DateTime.UtcNow.AddDays(15)));
+
+            return Task.CompletedTask;
         }
 
         public async Task ExecuteAsync(IFlowExecutionContext context)
@@ -23,6 +40,46 @@ namespace DidactCore
             logger.LogInformation("Starting work...");
             await Task.Delay(100);
             logger.LogInformation("Work completed.");
+
+            var someObject = new { httpAction = "POST", url = "someUrl" };
+            var someObjectStringified = JsonSerializer.Serialize(someObject);
+
+            var director = context.Director;
+            await director.CreateSubflowRunAsync(
+                flowName: "subflow",
+                jsonPayload: someObjectStringified,
+                executeAt: DateTime.Now);
+
+            await context.Director.ExecuteStepAsync(
+                name: "step-1",
+                function: async stepExecutionContext =>
+                {
+                    var logger = stepExecutionContext.Logger;
+                    logger.LogInformation("Executing step...");
+                    await Task.Delay(100);
+                    logger.LogInformation("Step execution completed.");
+                });
+
+            await context.Director.SuspendFor(name: "first-sleep", duration: TimeSpan.FromDays(5));
+            await context.Director.SuspendUntil(name: "second-sleep", resumeAt: DateTime.UtcNow.AddDays(10));
+
+            var step2result = await context.Director.ExecuteStepAsync(
+                name: "step-2",
+                function: async stepExecutionContext =>
+                {
+                    var logger = stepExecutionContext.Logger;
+                    logger.LogInformation("Executing step...");
+                    await Task.Delay(100);
+                    logger.LogInformation("Step execution completed.");
+                    return "Step 2 result";
+                });
+
+            await context.Director.ExecuteStepAsync(
+                name: "step-3",
+                function: stepExecutionContext =>
+                {
+                    return Task.CompletedTask;
+                });
         }
     }
 }
