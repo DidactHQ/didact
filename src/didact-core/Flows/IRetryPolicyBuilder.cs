9c66d1463157ed125e19712a88eedf3ef212f5ab
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-06 01:04:48 -0500

    Created IRetryPolicyBuilder

---
+++ src/didact-core/Flows/IRetryPolicyBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DidactCore.Flows
+{
+    public interface IRetryPolicyBuilder
+    {
+        IRetryPolicyBuilder WithMaxAttempts(int maxAttempts);
+
+        IRetryPolicyBuilder UseFixedInterval(TimeSpan delay);
+
+        IRetryPolicyBuilder UseExponentialBackoff(
+            TimeSpan initialDelay,
+            TimeSpan maxDelay,
+            double multiplier = 2);
+
+        IRetryPolicyBuilder UseLinearBackoff(
+            TimeSpan initialDelay,
+            TimeSpan maxDelay);
+
+        IRetryPolicyBuilder WithRetryWindow(TimeSpan retryWindow);
+
+        IRetryPolicyBuilder WithAttemptTimeout(TimeSpan timeout);
+    }
+}
