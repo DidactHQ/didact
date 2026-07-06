f0384edbf0bb91fbedb98f6e85ca09bd7149b648
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-06 01:06:16 -0500

    Created RetryStategies constants

---
+++ src/didact-primitives/Constants/RetryStrategies.cs
@@ -0,0 +1,10 @@
+﻿namespace DidactPrimitives.Constants
+{
+    public static class RetryStrategies
+    {
+        public const string None = "None";
+        public const string FixedInterval = "Fixed Interval";
+        public const string ExponentialBackoff = "Exponential Backoff";
+        public const string LinearBackoff = "Linear Backoff";
+    }
+}
