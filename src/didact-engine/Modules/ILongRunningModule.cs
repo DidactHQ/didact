161799d29ec704896057af4053d8f18ba7dc8261
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-22 17:54:54 -0500

    Add long-running module contract

---
+++ src/didact-engine/Modules/ILongRunningModule.cs
@@ -0,0 +1,9 @@
+namespace DidactEngine.Modules
+{
+    /// <summary>
+    /// A module whose run task represents a continuously blocking lifetime operation.
+    /// </summary>
+    public interface ILongRunningModule : IModule
+    {
+    }
+}
