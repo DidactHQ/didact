d45a3ffdfb1c71665297212d853b88618813ee7f
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2026-07-22 22:59:54 -0500

    Created ModuleStates for didact engine modules

---
+++ src/didact-primitives/Constants/ModuleStates.cs
@@ -0,0 +1,13 @@
+﻿namespace DidactPrimitives.Constants
+{
+    public static class ModuleStates
+    {
+        public const string Ready = "Ready";
+        public const string Running = "Running";
+        public const string Failing = "Failing";
+        public const string Failed = "Failed";
+        public const string Restarting = "Restarting";
+        public const string Stopping = "Stopping";
+        public const string Stopped = "Stopped";
+    }
+}
